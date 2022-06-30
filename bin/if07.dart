a716db7d611e7195e417f85f7f7170174cba4d48
ogabek <norpulatovogabek42@gmail.com>
2022-06-30 21:47:22 +0500

    task7

--- bin/if07.dart
+++ bin/if07.dart
@@ -1,7 +1,7 @@
 /*  
 Create function called func  
 Return the days of the week, return the days of the week according to the numbers 1 to 7.
-Use the elif statments.
+Use the else if statments.
 1: "Monday"
 2: "Tuesday"
 3: "Wednesday"
@@ -15,5 +15,33 @@ Returns:
     str: return answer. 
 */
 
+String func(day) {
+  String q, w, e, r, t, y, u;
+  q = 'Monday';
+  w = 'Tuesday';
+  e = 'Wednesday';
+  r = 'Thursday';
+  t = 'Friday';
+  y = 'Saturday';
+  u = 'Sunday';
+  if (day == 1) {
+    return q;
+  } else if (day == 2) {
+    return w;
+  } else if (day == 3) {
+    return e;
+  } else if (day == 4) {
+    return r;
+  } else if (day == 5) {
+    return t;
+  } else if (day == 6) {
+    return y;
+  } else if (day == 7) {
+    return u;
+  }
+  return '';
+}
+
 void main() {
+  print(func(7));
 }
