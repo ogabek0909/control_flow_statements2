3a9986fe6a8ca16804bacbface65790b3eb390c0
ogabek <norpulatovogabek42@gmail.com>
2022-06-30 20:16:58 +0500

    task5

--- bin/if05.dart
+++ bin/if05.dart
@@ -6,6 +6,34 @@ Args:
 Returns:
     int: return answer.
 */
+int func(a) {
+  int q, w, e, r, t;
+  q = a % 10;
+  a ~/= 10;
+  w = a % 10;
+  a ~/= 10;
+  e = a % 10;
+  a ~/= 10;
+  r = a % 10;
+  t = a ~/ 10;
+  if (q > w && q > e && q > r && q > t) {
+    return q;
+  }
+  if (w > q && w > e && w > r && w > t) {
+    return w;
+  }
+  if (e > w && e > q && e > r && e > t) {
+    return e;
+  }
+  if (r > w && r > e && r > q && r > t) {
+    return r;
+  }
+  if (t > w && t > e && t > r && t > q) {
+    return t;
+  }
+  return 11;
+}
 
 void main() {
+  print(func(29564));
 }
