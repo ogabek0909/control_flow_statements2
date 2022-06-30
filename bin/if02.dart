58a324f80f104161570aa7a58b1e49d57a8018a3
ogabek <norpulatovogabek42@gmail.com>
2022-06-30 19:39:25 +0500

    task2

--- bin/if02.dart
+++ bin/if02.dart
@@ -8,5 +8,16 @@ Args:
 Returns:
     int: return answer.
 */
-void main() {
+int func(a, b, c) {
+  if (a < b) {
+    if (a < c) {
+      return a;
+    } else
+      return c;
+  } else if (b < c) {
+    return b;
+  } else
+    return c;
 }
+
+void main() {}
